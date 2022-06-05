08df7eb1c97a9da20a0cae86d50278d1e0b657a5
PetrNovak69 <100471782+PetrNovak69@users.noreply.github.com>
2022-06-05 18:06:53 +0200

    Create treti_mocnina.c

---
+++ treti_mocnina.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+ {
+  int n,c;
+  printf ("Zadej číslo\n");
+  scanf("%d\n",&n);
+  
+  c=n*n*n;
+  printf ("Mocnina na třetí tvého čísla=%d\n",c);
+  scanf("%d\n",&c);
+  
+    return 0;
+}
